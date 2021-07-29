d695e3793abc13e159dfbb594912cc5ed3d3cda1
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2021-07-28 23:24:41 -0300

    Update Motors.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/Motors.java
@@ -8,37 +8,56 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Motors extends Motores implements InterfaceMotors {
 
-    private Telemetry telemetry = super.tlmtr;
-    private DcMotor fright = super.Fright;
-    private DcMotor fleft = super.Fleft;
-    private ElapsedTime lp;
+    // this variable is responsible of connect the variables for class "MotorsHwmap" for class "Motors"
+    private Telemetry telemetry = super.tlmtr; 
+    private DcMotor fright = super.Fright; 
+    private DcMotor fleft = super.Fleft; 
+    
+    // this variable is responsible for Elapsed time
+    private ElapsedTime lp; 
 
     public void fright(){
-        fright.setDirection(DcMotor.Direction.FORWARD);
-
-        fright.setPower(1);
+        // this variable is responsible for the direction of motor
+        fright.setDirection(DcMotor.Direction.FORWARD);  
+        
+        // this variable is responsible for power of motor
+        fright.setPower(1);  
+        
+        // this variable is responsible for detect error and to send for console
         try {
             lp.wait(10);
         } catch (InterruptedException e) {
             telemetry.addData("error: ", fright.getConnectionInfo());
         }
+                         
+        // this variable is responsible for power of motor
         fright.setPower(0);
     }
     public void fleft(){
+        // this variable is responsible for the direction of motor
         fleft.setDirection(DcMotor.Direction.REVERSE);
+        
+        // this variable is responsible for power of motor
         fleft.setPower(1);
+        
+        // this variable is responsible for detect error and to send for console
         try {
-            lp.wait();
+            // this variable is responsible for the elapsed time of the report.
+            lp.wait(10); 
         } catch (InterruptedException e) {
             telemetry.addData("Error: ", fleft.getConnectionInfo());
         }
+        
+        // this variable is responsible for power of motor
         fleft.setPower(0);
 
     }
     public void activated(){
+        // this variable is responsible for checking if the motor is activated. If the engine power is greater than 1, the engine is activated
         if(fleft.getPower() > 1){
             telemetry.addData("MotorL", "Activated!");
             telemetry.update();
+            
              if(fright.getPower() > 1){
               telemetry.addData("MotorR", "Activated!");
               telemetry.update();
@@ -46,17 +65,15 @@ public class Motors extends Motores implements InterfaceMotors {
         }
     }
     public void off(){
+        // this variable is responsible for checking if it is disabled. If the engine power is less than 1, the engine is disabled
         if(fleft.getPower() == 0){
             telemetry.addData("MotorL", "Disabled!");
             telemetry.update();
+            
             if(fright.getPower() == 0){
                 telemetry.addData("MotorR", "Disabled!");
                 telemetry.update();
             }
         }
     }
-
-
 }
-
-
