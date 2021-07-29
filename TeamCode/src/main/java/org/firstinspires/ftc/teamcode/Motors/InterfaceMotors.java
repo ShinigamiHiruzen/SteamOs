24a8a93e43d094cffce99099970379080c16d17d
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2021-07-29 00:00:08 -0300

    Delete InterfaceMotors.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/InterfaceMotors.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.Motors;
-
-public interface InterfaceMotors {
-
-  void activated(); // this variable is responsible for check if the engine is running
-  void off(); // this variable is responsible for check if the engine disabled
-}
