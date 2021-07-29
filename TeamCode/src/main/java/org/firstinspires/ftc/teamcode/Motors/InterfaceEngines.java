685b79f9830d3f7dc5391871b9a35ed7a5d28138
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2021-07-28 23:59:20 -0300

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/InterfaceEngines.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Motors;
+
+public interface InterfaceEngines {
+
+  void activated(); // this variable is responsible for check if the engine is running
+  void off(); // this variable is responsible for check if the engine disabled
+}
