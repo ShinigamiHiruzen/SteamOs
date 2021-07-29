31da804af0da38fdaa8750a59dda81b0ef3d9e96
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2021-07-28 23:29:08 -0300

    Update InterfaceMotors.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/InterfaceMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/InterfaceMotors.java
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode.Motors;
 
 public interface InterfaceMotors {
 
-  void activated(); // this variable is responsible for check if the motor is running
-  void off(); // this variable is responsible for check if the motor disabled
+  void activated(); // this variable is responsible for check if the engine is running
+  void off(); // this variable is responsible for check if the engine disabled
 }
