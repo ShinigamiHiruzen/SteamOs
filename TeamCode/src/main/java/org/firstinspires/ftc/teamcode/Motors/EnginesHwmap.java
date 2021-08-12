6746f6844c42dc25f4e939bb5b7ea1659d7ba1fb
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2021-08-12 20:55:38 -0300

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors/EnginesHwmap.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Motors;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class EnginesHwmap {
+
+    protected DcMotor Fright; //this variable is responsible for right engine
+    protected DcMotor Fleft; //this variable is responsible for left engine
+    private HardwareMap hwmap; //this variable is responsible for hardware map
+    protected Telemetry tlmtr; //this variable is responsible for telemetry
+
+    public void Sethardware() {
+
+        // this variables is responsible for connect engines to hardware map
+        Fright = hwmap.dcMotor.get("Right motor"); 
+        Fleft = hwmap.dcMotor.get("Left motor"); 
+       
+     }
+}
